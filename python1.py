2a6e6bb8c97d726489456c87aed1b45c3fa3978b
achmad321 <110509186+achmad321@users.noreply.github.com>
2022-08-07 18:52:58 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+# belajar output
+
+print("hello world")
+print("nama saya dery")
+print("hobby saya bermain futsal")
+print(" saya suka jajan jus")
+print("dan saya suka bertanding futsal")
+print("saya tinggal di areman")
+print("kalo sakit hati nambah pikiran hidup kelazzzz")
+print("saya makan kalo laperr")
+print("saya suka tidur juga")
+print("sya suka sholat karena sholat adalah jalan menuju ke suksessan") 
