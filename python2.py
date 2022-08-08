60aa96984a544154d380888d4c85b31a857c625b
achmad321 <110509186+achmad321@users.noreply.github.com>
2022-08-07 19:00:31 -0700

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,11 @@
+python2.py 
+
+nama= "dery" =dery adalah data, nama adalah variabel 
+umur= 15
+alamat= "cimanggis,kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("Nama saya adalah " + nama) 
