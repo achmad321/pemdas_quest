1e15d3335c8c392a6293d3f572aeaaf34d1133fc
achmad321 <110509186+achmad321@users.noreply.github.com>
2022-08-12 10:51:10 +0700

    Add files via upload

---
+++ uprak_dery raihan a X TKJ 1.py
@@ -0,0 +1,12 @@
+# tugas uprak pemrograman dasar
+
+
+nama = input("masukkan nama anda:" )
+nama =input("masukan nama belakang anda:" )
+kelas= input("masukan kelas anda:" ) 
+print("nama anda adalah", Nama, nama ,"dan anda kelas", kelas)
+
+
+suhu = int(input("masukan suhu celcius"))
+reamur = (4/5 * suhu)
+print ("suhu, celcius =" , reamur, "reamur")
